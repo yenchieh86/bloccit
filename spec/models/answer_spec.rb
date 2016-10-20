bd3bbde8304177ec33c793bd8a2a0132f1cc5582
Yen <yenchieh86@hotmail.com>
2016-10-20 00:23:08 -0700

    CP15-assignment: Finished

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+    let(:question) { Question.create!(title: "New Post Title", body: "New Post Body", resolved: false)}
+    let(:answer) {Answer.create!(body: "New Post Body")}
+  
+    describe "attribues" do
+        it "has a body attributes" do
+            expect(answer).to have_attributes(body: "New Post Body")
+        end
+    end
+end
